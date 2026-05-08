d015a9d49e3d3a414d5f3c398d0ead9b3243b9e1
rachadaccoumeh <rachadaccoumeh@gmail.com>
2026-05-08 09:28:00 +0300

    Update package and bundle identifiers to dev.ubity.SWrkT

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "dev.ubity.SWrkT.swrkt"
+    namespace = "dev.ubity.SWrkT"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "dev.ubity.SWrkT.swrkt"
+        applicationId = "dev.ubity.SWrkT"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/dev/ubity/SWrkT/MainActivity.kt
+++ android/app/src/main/kotlin/dev/ubity/SWrkT/MainActivity.kt
@@ -1,4 +1,4 @@
-package dev.ubity.SWrkT.swrkt
+package dev.ubity.SWrkT
 
 import io.flutter.embedding.android.FlutterActivity
 
